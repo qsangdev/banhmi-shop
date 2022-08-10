2d9a2d2a765eb8d921fb4de948cec65e7a0951f8
qsangdev <quangsang02018@gmail.com>
2022-08-10 14:37:23 +0700

    css update

--- app.js
+++ app.js
@@ -1,30 +1,13 @@
-// <<<<<<< HEAD
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
-
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("slideshow");
-  if (n > slides.length) {
-    slideIndex = 1;
+var counter = 1;
+setInterval(function () {
+  document.getElementById("radio" + counter).checked = true;
+  counter++;
+  if (counter > 4) {
+    counter = 1;
   }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex - 1].style.display = "block";
-}
-// =======
+}, 5000);
+
+// ============================
 
 let menu = document.querySelector(".menu_items");
 
